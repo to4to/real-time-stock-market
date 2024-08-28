9983f146dc19ce30d36dd186a058244d8d14bcf2
to4to <justjustifying@outlook.com>
2024-08-28 14:46:45 +0530

    chore: Add websocket functionality to server
    The code changes in this commit add websocket functionality to the server. It upgrades the incoming GET request into a websocket connection and implements the logic to read and write messages using the websocket connection. This allows for real-time communication between the server and clients.

--- server/main.go
+++ server/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
@@ -17,9 +19,41 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	//Todo : Upgrade he incoming GET request into a webocket connection
 
-	conn, err := upgrader.Upgrade()
+	conn, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer conn.Close()
+
+	for {
+
+		messageType, message, err := conn.ReadMessage()
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf("Received: %s\n", message)
+
+		res := fmt.Sprintf("%s , at: %s", string(message), time.Now().String())
+
+		if err := conn.WriteMessage(messageType, []byte(res)); err != nil {
+			fmt.Println(err)
+			return
+
+		}
+	}
 
 }
 func main() {
 	http.HandleFunc("/ws", handler)
+
+	fmt.Println("starting Server On :3000")
+
+	if err := http.ListenAndServe(":3000", nil); err != nil {
+		fmt.Println(err)
+	}
 }
