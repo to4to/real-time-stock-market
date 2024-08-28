179a93b3429a88cdb59018a5397837651a3fe9b0
to4to <justjustifying@outlook.com>
2024-08-28 16:19:44 +0530

    feat: Add websocket functionality to client
    This commit adds websocket functionality to the client code. It includes the necessary imports, sets up a goroutine for reading messages from the websocket connection, and another goroutine for sending messages to the server. It also adds a scanner to read input from the terminal and send it to the websocket server.

--- client/main.go
+++ client/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"net/url"
+	"os"
 
 	"github.com/gorilla/websocket"
 )
@@ -33,4 +35,42 @@ func main() {
 
 	done := make(chan struct{})
 
+	// Go Routine Reading messages
+
+	go func() {
+
+		for {
+			_, message, err := conn.ReadMessage()
+			if err != nil {
+				log.Println("read: ", err)
+				return
+			}
+
+			fmt.Printf("Received:  %s\n", message)
+		}
+
+	}()
+
+	//Go routine for sending message
+
+	go func() {
+
+		for {
+
+		}
+
+	}()
+	// Read input with the terminal and send it to the websocket server
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("type Something")
+
+	for scanner.Scan() {
+		text := scanner.Text()
+		//send text to channel
+		send <- Message{websocket.TextMessage, []byte(text)}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Println("Scanner err: ", err)
+	}
 }
